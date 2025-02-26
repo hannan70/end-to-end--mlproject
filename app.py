c5090fa6f4f57debb1a803e26f832d9ff4866c26
hannan70 <hannan728070@gmail.com>
2025-02-26 17:09:57 +0600

    prediction

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+
+application = Flask(__name__)
+
+app = application
+
+@app.route("/")
+def index_page():
+    return render_template('index.html') 
+
+
+@app.route("/predict", methods=['GET', 'POST'])
+def predict():
+    if request.method == 'GET':
+        return render_template("index.html")
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        pred_df=data.get_data_as_data_frame()
+    
+        predict_pipeline=PredictPipeline()
+ 
+        results=predict_pipeline.predict_data(pred_df)
+
+        formated_result = f"{results[0]:.2f}"
+  
+        return render_template('index.html',results=formated_result)
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")   
+
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score 
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 def evaluate_models(X_train, X_test, y_train, y_test, models, param):
@@ -48,4 +49,13 @@ def save_object(file_path, model):
             dill.dump(model, file_obj)
 
     except CustomException as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
         raise CustomException(e, sys)
